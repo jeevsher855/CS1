78a7a70d211d89a49ff23c7c6bad33ae96e68328
jeevsher855 <jeevanshergill2007@gmail.com>
2025-12-08 16:23:31 -0800

    ASCII BINARY DONE

--- ASCII_Table_Demonstration/ASCII_Binary.cpp
+++ ASCII_Table_Demonstration/ASCII_Binary.cpp
@@ -1,5 +1,8 @@
+// Jeevan Shergill & Christopher Xiao
+
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -16,13 +19,14 @@ int main() {
     ASCII a[SIZE];
     int pos = 0;
 
-    fstream ASCIIfile;
+    fstream ASCIIfile ("ASCII.dat", ios::out | ios::binary);
 
-    calcASCII( a, '0', '9', pos);
+    calcASCII (a, '0', '9', pos);
     calcASCII (a, 'A', 'Z', pos);
     calcASCII (a, 'a', 'z', pos);
 
     print(a, pos, ASCIIfile);
+    return 0;
 }
 
 void calcASCII(ASCII array[], char start, char end, int& pos) {
@@ -35,18 +39,21 @@ void calcASCII(ASCII array[], char start, char end, int& pos) {
 }
 
 void print(ASCII array[], int pos, fstream& file) {
-    cout << "Character\tASCII Table\n";
+
+    ASCII temp;
+
+    cout << "Character" << setw(15) << "ASCII Table\n";
     cout << "-----------------------\n";
-    for (int count = 0; count < pos; count++) {
-        file.write(reinterpret_cast<char*>(&array), sizeof(array));
-        file.close();
-        file.open("ASCII.dat", ios::in | ios::binary | ios::app);
-        file.read(reinterpret_cast<char*>(&array), sizeof(array));
-        file.close();
-        file.open("ASCII.dat", ios::out | ios::binary | ios::app);
+
+    for (int count = 0; count < pos; count++)
+        file.write(reinterpret_cast<char*>(&array[count]), sizeof(array));
+
+    file.close();
+    file.open("ASCII.dat", ios::in | ios::binary);
+
+    while (file.read(reinterpret_cast<char*>(&temp), sizeof(temp)))
+    {
+        cout << temp.letter << setw(17) << temp.number << endl;
     }
 
-    for (int count = 0; count < pos; count++) {
-        cout << array[count].letter << "\t\t\t" << array[count].number << "\n";
-    }
 }
